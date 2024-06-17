cebdc56b9c97ff779957a92328beaa8e5a3ee3fe
amazzetta <aaron.mazzetta@gmail.com>
2024-06-17 14:35:46 +0200

    solved problem 24

--- src/problems/problem_024.cpp
+++ src/problems/problem_024.cpp
@@ -1,9 +1,40 @@
 #include "euler_utils.h"
 
+std::string get_nth_permutation(std::string& s, int n)
+{
+    std::sort(s.begin(), s.end());
+    n -= 1;
+    const size_t size = s.size();
+    std::string permutation = "";
+    for ( int i = 0; i < size; ++i ) {
+        size_t f = runtime_small_factorial(size - 1 - i);
+        size_t idx = n / f;
+        n %= f;
+        permutation += s[idx];
+        s.erase(s.begin() + idx);
+    }
+    return permutation;
+}
+
 size_t solve_problem_24()
 {
-    size_t result = 0;
+    std::string s = "0123456789";
+    const int n = 1000000;
+    const std::string nth_perm = get_nth_permutation(s, n);
+    size_t result = std::stoull(nth_perm);
+
+    ASSERT_EQUAL(result, 2783915460);
     return result;
 }
 
 REGISTER_PROBLEM(solve_problem_24, 24)
+
+
+/*
+012
+021
+102
+
+
+
+*/
