a71584299cb4dff1917c3bb98f561a14fbf7a5b5
amazzetta <aaron.mazzetta@gmail.com>
2024-06-17 14:08:56 +0200

    solved problem 22

---
+++ src/problems/problem_022.cpp
@@ -0,0 +1,56 @@
+#include "euler_utils.h"
+#include <fstream>
+
+std::vector<std::string> read_input()
+{
+    const std::string path = "../inputs/0022_names.txt";
+    std::ifstream file(path);
+    if ( !file.is_open() ) {
+        std::cerr << "Failed to open the file." << std::endl;
+        return {};
+    }
+
+    std::string line;
+    std::vector<std::string> names;
+
+    if ( std::getline(file, line) ) {
+        std::stringstream ss(line);
+        std::string name;
+
+        while ( std::getline(ss, name, ',') ) {
+            name.erase(remove(name.begin(), name.end(), '"'), name.end());
+
+            for ( char& c : name ) {
+                if ( c >= 'A' ) {
+                    c = ('a' + (c - 'A'));
+                }
+            }
+
+            names.push_back(name);
+        }
+    }
+
+    file.close();
+
+    std::sort(names.begin(), names.end());
+    return names;
+}
+
+size_t solve_problem_22()
+{
+    const auto names = read_input();
+    const size_t size = names.size();
+    size_t result = 0;
+    for ( size_t i = 0; i < size; ++i ) {
+        size_t score = 0;
+        for ( char c : names[i] ) {
+            score += (size_t) (c - 'a' + 1);
+        }
+        result += score * (i+1);
+    }
+
+    ASSERT_EQUAL(result, 871198282);
+    return result;
+}
+
+REGISTER_PROBLEM(solve_problem_22, 22)
