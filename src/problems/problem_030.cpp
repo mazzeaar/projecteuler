219196ac2fe9ecb2c356dc55094dcde15f9fe5a2
amazzetta <aaron.mazzetta@gmail.com>
2024-06-20 11:22:21 +0200

    solved problem 30

--- src/problems/problem_030.cpp
+++ src/problems/problem_030.cpp
@@ -2,7 +2,29 @@
 
 i64 solve_problem_30()
 {
+    const size_t N = 5;
+    std::array<i64, 10> powers;
+    i64 upper_bound = 0;
+    for ( int i = 0; i <= 10; ++i ) {
+        powers[i] = std::pow(i, N);
+        upper_bound += powers[i];
+    }
+
     i64 result = 0;
+    for ( i64 i = 2; i < upper_bound; ++i ) {
+        i64 sum = 0;
+        i64 num = i;
+        while ( num != 0 ) {
+            sum += powers[num % 10];
+            num /= 10;
+        }
+
+        if ( sum == i ) {
+            result += i;
+        }
+    }
+
+    ASSERT_EQUAL(result, 443839);
     return result;
 }
 
