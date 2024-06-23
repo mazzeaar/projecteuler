4afcd96e8db5c9f3989ba0578f7f636498366728
amazzetta <aaron.mazzetta@gmail.com>
2024-06-23 16:38:29 +0200

    added include <numeric> and new function

--- include/euler_utils.h
+++ include/euler_utils.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 
+#include <numeric>
 #include <cmath>
 #include <cstdint>
 
--- include/utils/math.h
+++ include/utils/math.h
@@ -1,6 +1,16 @@
 #ifndef MATH_H
 #define MATH_H
 
+namespace math {
 
+    // counts the number of digits in a given i64
+    inline size_t count_digits(i64 n)
+    {
+        size_t d = 0;
+        for ( ; n != 0; n /= 10 ) ++d;
+        return d;
+    }
+
+} // namespace math
 
 #endif // MATH_H
